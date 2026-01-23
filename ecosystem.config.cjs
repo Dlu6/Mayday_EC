8fd79f0a9ca104dc220c798261836a09736583f2
Dlu6 <medhi.mattovu@gmail.com>
2026-01-23 12:57:28 +0300

    fix: Update ecosystem.config.cjs to use HPE server IP (192.168.1.15) for CORS

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -9,7 +9,7 @@ module.exports = {
       env: {
         NODE_ENV: "production",
         PORT: 8004,
-        PUBLIC_IP: "192.168.1.14",
+        PUBLIC_IP: "192.168.1.15",
         ASTERISK_CONFIG_PATH: "/etc/asterisk",
         LICENSE_MGMT_API_URL: "https://mayday-website-backend-c2abb923fa80.herokuapp.com/api",
         SECRET_INTERNAL_API_KEY: "aVeryLongAndRandomSecretStringForInternalComms_987654321_production",
